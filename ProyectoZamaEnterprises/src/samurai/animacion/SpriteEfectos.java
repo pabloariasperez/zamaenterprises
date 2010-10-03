2c0a83c1c833836216e848681ceb4711bdd451f8
amuchaonra <amuchaonra@e0425a37-91e3-9a09-17b8-ceb9f1400c81>
2010-10-03 10:50:15 +0000

    Se logro la animacion del sprite de la espada

--- ProyectoZamaEnterprises/src/samurai/animacion/SpriteEfectos.java
+++ ProyectoZamaEnterprises/src/samurai/animacion/SpriteEfectos.java
@@ -31,7 +31,7 @@ public class SpriteEfectos extends Sprite implements Animable {
 
         this.secuenciaIzq=new int[] {0,0,1,1,2,2,3,3,4,4,5,5,6,6};
         this.secuenciaDer=new int[] {7,7,8,8,9,9,10,10,11,11, 12,12,13,13};
-        this.secuenciaFrente=new int[] {14,14,14,14,14,15,15,15,15,16,16,16,16,16};
+        this.secuenciaFrente=new int[] {14,14,14,14,15,15,15,15,15,15,16,16,16,16};
     }
 
     public void dibujar(Graphics g) {
