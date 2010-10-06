4b57733a2601e070ffc1aed2b8fb662f299f6ee4
layzam7 <layzam7@e0425a37-91e3-9a09-17b8-ceb9f1400c81>
2010-10-06 15:54:30 +0000

    La linea que corta a los botones del menu fue implementada

--- trunk/ProyectoZamaEnterprises/src/samurai/menu/Indicador.java
+++ trunk/ProyectoZamaEnterprises/src/samurai/menu/Indicador.java
@@ -10,18 +10,29 @@ public class Indicador extends Sprite {
 
     private int posicionX;
     private int posicionY;
+    private int crecimientoLinea;
     public Indicador(String archivoIndicador, int posicionX, int posicionY) throws IOException{
         super(Image.createImage(archivoIndicador),135,40);
         this.posicionX=posicionX;
         this.posicionY=posicionY;
         this.setPosition(posicionX,posicionY);
+        this.crecimientoLinea=0;
     }
     public void dibujar(Graphics g){
-        this.paint(g);
+        if(this.crecimientoLinea<=240){
+            g.drawLine(0, posicionY+20, posicionX+this.crecimientoLinea, posicionY+20);
+            this.crecimientoLinea+=16;
+        }else if(this.crecimientoLinea>240&&this.crecimientoLinea<260){
+            g.fillRect(0, posicionY+17, posicionX+this.crecimientoLinea,6 );
+            this.crecimientoLinea++;
+        }else{
+            this.paint(g);
+        }
     }
     public void cambiarPosicion(int nuevaPosicionX,int nuevaPosicionY){
         this.posicionX=nuevaPosicionX;
         this.posicionY=nuevaPosicionY;
         this.setPosition(posicionX, posicionY);
+        this.crecimientoLinea=0;
     }
 }
