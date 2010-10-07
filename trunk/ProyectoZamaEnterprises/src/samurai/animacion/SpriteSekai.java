4a1898532092a3c52e8b4ee236293a7b90c94283
layzam7 <layzam7@e0425a37-91e3-9a09-17b8-ceb9f1400c81>
2010-10-07 04:05:18 +0000

    se cambio la imagen de sekai y se animo

--- trunk/ProyectoZamaEnterprises/src/samurai/animacion/SpriteSekai.java
+++ trunk/ProyectoZamaEnterprises/src/samurai/animacion/SpriteSekai.java
@@ -7,14 +7,18 @@ import javax.microedition.lcdui.game.Sprite;
 
 public class SpriteSekai extends Sprite implements Animable {
     private int posicionX,posicionY;
+    int[] secuenciaCorrer;
+    private int retrasoAnimacion;
+    private final int salto;
 
     public SpriteSekai(String archivoSekai, int posicionX, int posicionY) throws IOException{
 
         super(Image.createImage(archivoSekai),40,60);
         this.posicionX = posicionX;
         this.posicionY = posicionY;
-        //int[] secuencia = {};
-
+        this.secuenciaCorrer = new int[] {0,1};
+        this.retrasoAnimacion=0;
+        this.salto=10;
 
        //this.setFrameSequence(secuencia);
 
@@ -27,9 +31,16 @@ public class SpriteSekai extends Sprite implements Animable {
     }
 
     public void actualizar() {
-
-        this.nextFrame();
-
+        if(retrasoAnimacion%8==0){
+            if(this.getFrame()==0){
+                this.setPosition(posicionX, posicionY+salto);
+                this.nextFrame();
+            }else{
+                this.setPosition(posicionX, posicionY);
+                this.nextFrame();
+            }
+        }
+        retrasoAnimacion++;
     }
 
 }
