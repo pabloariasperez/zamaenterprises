1461afa718ec174ea4b15abceb40442ff8250f4a
layzam7 <layzam7@e0425a37-91e3-9a09-17b8-ceb9f1400c81>
2010-10-21 18:25:08 +0000

    comentarios Javadoc

--- trunk/ProyectoZamaEnterprises/src/samurai/juego/Actualizable.java
+++ trunk/ProyectoZamaEnterprises/src/samurai/juego/Actualizable.java
@@ -6,17 +6,17 @@
 package samurai.juego;
 
 /**
- *
- * @author Pablo
- * Interface que obliga a que se deba actualizar y a que regresen un Graphics los "BICHOS" de una clase.
+ * Interface que obliga a que se deba actualizar y a que regresen un Graphics.
+ * @author Pablo, Erik, Daniel
+ * @version 1.0 Octubre 2010
  */
 public interface Actualizable {
     /**
-     *
+     * Actualiza
      */
     public abstract void actualizar();
     /**
-     *
+     * Dibuja
      */
     public abstract void dibujar();
 }
