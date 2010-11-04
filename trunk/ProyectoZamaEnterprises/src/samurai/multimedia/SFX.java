74afded2358ec59a817f0b1d3c2f4f1e965f6c4c
layzam7 <layzam7@e0425a37-91e3-9a09-17b8-ceb9f1400c81>
2010-11-04 06:06:35 +0000

    se cambio juego , nivel y sfx

--- trunk/ProyectoZamaEnterprises/src/samurai/multimedia/SFX.java
+++ trunk/ProyectoZamaEnterprises/src/samurai/multimedia/SFX.java
@@ -6,18 +6,18 @@
 package samurai.multimedia;
 
 import java.io.IOException;
-import java.util.Vector;
 import javax.microedition.media.Manager;
 import javax.microedition.media.MediaException;
 import javax.microedition.media.Player;
+import javax.microedition.midlet.MIDlet;
 
 /**
  *
  * @author zama
  */
 public class SFX {
-    private Vector players;
-    private Vector posiciones;
+    private Player[] players;
+    private MIDlet samuraiMidlet;
 
     public static final int ESPADA=0;
     public static final int GOLPE_SEKAI=1;
@@ -29,63 +29,54 @@ public class SFX {
     public static final int MUERTE_CESAR=7;
 
 
-    public SFX(){
-        this.players= new Vector();
+    public SFX(MIDlet samuraiMidlet){
+        this.samuraiMidlet=samuraiMidlet;
+        this.players= new Player[8];
     }
     public void agregarSFX(int tipo){
-        Player p = null;
         try {
             switch(tipo){
                 case SFX.ESPADA:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/espada.mp3"), "audio/mp3");
+                    players[tipo]=Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/espada.mp3"), "audio/mp3");
                     break;
                 case SFX.GOLPE_SEKAI:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/golpe.mp3"), "audio/mp3");
+                    players[tipo]= Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/golpe.mp3"), "audio/mp3");
                     break;
                 case SFX.MUERTE_SEKAI:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/muerte.mp3"), "audio/mp3");
+                    players[tipo]= Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/muerte.mp3"), "audio/mp3");
                     break;
                 case SFX.MUERTE_MURCIELAGO:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/murcielago.mp3"), "audio/mp3");
+                    players[tipo]= Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/murcielago.mp3"), "audio/mp3");
                     break;
                 case SFX.MUERTE_TOPO:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/topo.mp3"), "audio/mp3");
+                    players[tipo]= Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/topo.mp3"), "audio/mp3");
                     break;
                 case SFX.MUERTE_RATA:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/rata.mp3"), "audio/mp3");
+                    players[tipo] = Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/rata.mp3"), "audio/mp3");
                     break;
                 case SFX.MUERTE_FANTASMA:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/fantasma.mp3"), "audio/mp3");
+                    players[tipo] = Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/fantasma.mp3"), "audio/mp3");
                     break;
                 case SFX.MUERTE_CESAR:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/cesar.mp3"), "audio/mp3");
+                    players[tipo] = Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/cesar.mp3"), "audio/mp3");
                     break;
                 default:
-                    p = Manager.createPlayer(getClass().getResourceAsStream("/wrong.mp3"), "audio/mp3");
+                    players[tipo] = Manager.createPlayer(this.samuraiMidlet.getClass().getResourceAsStream("/wrong.mp3"), "audio/mp3");
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (MediaException ex) {
             ex.printStackTrace();
         }
-        if( p != null){
-            this.players.addElement(p);
-        }
-        this.posiciones.addElement(new Integer(tipo));
     }
     public void reproducir(int sfx){
-        int numeroPlayers = players.size();
-        for( int posicionP = 0; posicionP < numeroPlayers; posicionP++ ){
-            if( ((Integer)posiciones.elementAt(posicionP)).intValue() == sfx){
-                try {
-                    Player p = ((Player) players.elementAt(posicionP));
-                    p.realize();
-                    p.prefetch();
-                    p.start();
-                } catch (MediaException ex) {
-                    ex.printStackTrace();
-                }
-            }
+        try {
+            Player p = players[sfx];
+            p.realize();
+            p.prefetch();
+            p.start();
+        } catch (MediaException ex) {
+            ex.printStackTrace();
         }
     }
 }
