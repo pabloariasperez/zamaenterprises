3f3bbbf736416ea9a50695f0827debb533f4d079
lambsito <lambsito@e0425a37-91e3-9a09-17b8-ceb9f1400c81>
2010-11-25 02:11:59 +0000

    Revisión AdministradorData

--- trunk/ProyectoZamaEnterprises/src/samurai/almacenamiento/AdministradorData.java
+++ trunk/ProyectoZamaEnterprises/src/samurai/almacenamiento/AdministradorData.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package samurai.almacenamiento;
 
 import javax.microedition.rms.RecordStore;
@@ -14,124 +13,143 @@ import javax.microedition.rms.RecordStoreException;
  */
 public class AdministradorData {
 
-    private RecordStore rs;
-    private String nombreData;
+    private RecordStore recordStore;
+    private String nombreStore;
     public static final String SVacio = "vacio";
-    public static final int REGISTRO_SCORE_ACTUAL=1;
-    public static final int REGISTRO_VIDA=2;
-    public static final int REGISTRO_NIVEL=3;
-    public static final int REGISTRO_TIEMPO=4;
+    public static final int REGISTRO_SCORE_ACTUAL = 1;
+    public static final int REGISTRO_VIDA = 2;
+    public static final int REGISTRO_NIVEL = 3;
+    public static final int REGISTRO_TIEMPO = 4;
 
-    public AdministradorData(String nombreData){
-        this.nombreData = nombreData;
+    public AdministradorData(String nombreStore) {
+        this.nombreStore = nombreStore;
         try {
-            rs = RecordStore.openRecordStore(this.nombreData, true);
-            rs.closeRecordStore();
-        } catch (RecordStoreException ex) {
-            ex.printStackTrace();
-        }
-    }
-    public void cambiarTabla(String nombreData){
-        this.nombreData = nombreData;
-         try {
-            rs = RecordStore.openRecordStore(this.nombreData, true);
-            rs.closeRecordStore();
+            //Abrimos el Store y pedimos al método que lo cree si aún no existe.
+            recordStore = RecordStore.openRecordStore(this.nombreStore, true);
+            //LO cerramos. El objetivo de abrirlo y cerrarlo es simplemente asegurarnos de que existe.
+            recordStore.closeRecordStore();
         } catch (RecordStoreException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void agregarRegistro(String registro){
-        try{
-        rs = RecordStore.openRecordStore(this.nombreData, false);
-         byte data[] = registro.getBytes();
-         rs.addRecord(data, 0, data.length);
-          rs.closeRecordStore();
-            
-      } catch (RecordStoreException e) {
-          e.printStackTrace();
-      } 
-    }
-    public void agregarRegistro(int registro){
-        try{
-        rs = RecordStore.openRecordStore(this.nombreData, false);
-         byte data[] = {(byte)registro, (byte)20};
-         rs.addRecord(data, 0, data.length);
-          rs.closeRecordStore();
-
-      } catch (RecordStoreException e) {
-          e.printStackTrace();
-      }
-    }
-      public void cambiarRegistro(String registro, int index){
-        try{
-        rs = RecordStore.openRecordStore(this.nombreData, false);
-         byte data[] = registro.getBytes();
-         rs.addRecord(data, 0, data.length);
-         rs.setRecord(index, data, 0, data.length);
-         rs.closeRecordStore();
-
-      } catch (RecordStoreException e) {
-          e.printStackTrace();
-      } 
+    public void agregarRegistro(String valorRegistro) {
+        try {
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
+            byte datosRegistro[] = valorRegistro.getBytes();
+            //OFFSET el índice dentro de lso datos del buffer del primer byte relevante en el record.
+            recordStore.addRecord(datosRegistro, 0, datosRegistro.length);
+            recordStore.closeRecordStore();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String regresarRegistroCompleto(){
+    public void agregarRegistro(int valorRegistro) {
+        try {
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
+            byte datosRegistro[] = intToBytes(valorRegistro);
+            recordStore.addRecord(datosRegistro, 0, datosRegistro.length);
+            recordStore.closeRecordStore();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cambiarRegistro(String nuevoDato, int index) {
+        try {
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
+            byte datoRegistro[] = nuevoDato.getBytes();
+            recordStore.setRecord(index, datoRegistro, 0, datoRegistro.length);
+            recordStore.closeRecordStore();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cambiarRegistro(int nuevoDato, int index) {
+        try {
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
+            byte datoRegistro[] = intToBytes(nuevoDato);
+            recordStore.setRecord(index, datoRegistro, 0, datoRegistro.length);
+            recordStore.closeRecordStore();
+        } catch (RecordStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String regresarRegistroCompleto() {
         String registro = "";
         try {
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
 
-            rs = RecordStore.openRecordStore(this.nombreData, false);
-
-            for (int i = 1; i < rs.getNextRecordID(); i++) {
-                registro += new String(rs.getRecord(i));
+            for (int i = 1; i < recordStore.getNextRecordID(); i++) {
+                registro += new String(recordStore.getRecord(i));
             }
-            rs.closeRecordStore();
+            recordStore.closeRecordStore();
         } catch (RecordStoreException ex) {
             ex.printStackTrace();
-        } 
+        }
         return registro;
     }
-    public String regresarDato(int index){
+
+    public String regresarDato(int index) {
         String dato = "";
         try {
-            rs = RecordStore.openRecordStore(this.nombreData, false);
-            if(index>0 && index<=rs.getNumRecords()){
-            dato = new String(rs.getRecord(index));
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
+            if (index > 0 && index <= recordStore.getNumRecords()) {
+                dato = new String(recordStore.getRecord(index));
+            } else {
+                dato = AdministradorData.SVacio;
             }
-             else{
-             dato = AdministradorData.SVacio;
-             }
-            rs.closeRecordStore();
+            recordStore.closeRecordStore();
         } catch (RecordStoreException ex) {
             ex.printStackTrace();
         }
         return dato;
     }
 
-    public int regresarValorDato(int index){
-        int dato=-1;
-       try {
-            rs = RecordStore.openRecordStore(this.nombreData, false);
-            if(index>0 && index<=rs.getNumRecords()){
-            dato = (int)(rs.getRecord(index)[0]);
-            }
-             else{
-             dato = -1;
-             }
-            rs.closeRecordStore();
-        } catch (RecordStoreException ex) {
-            ex.printStackTrace();
-        }
-        return dato;
-    }
-
-    public void borrarTodo(){
+    public int regresarValorDato(int index) {
+        int valor = -1;
         try {
-           RecordStore.deleteRecordStore(nombreData);
-           rs = RecordStore.openRecordStore(nombreData, true);
-           rs.closeRecordStore();
+            recordStore = RecordStore.openRecordStore(this.nombreStore, false);
+            if (index > 0 && index <= recordStore.getNumRecords()) {
+                valor = bytesToInt( recordStore.getRecord(index) );
+            }
+            
+            recordStore.closeRecordStore();
         } catch (RecordStoreException ex) {
             ex.printStackTrace();
         }
+        return valor;
+    }
+
+    public void borrarTodo() {
+        try {
+            RecordStore.deleteRecordStore(nombreStore);
+        } catch (RecordStoreException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private byte[] intToBytes(int entero) {
+        byte bytes[] = new byte[4];
+        for (int i = 0; i < 3; i++) {
+            bytes[i] = (byte) entero;
+            entero >>= 8;
+        }
+        bytes[3] = (byte) entero;
+
+        return bytes;
+    }
+
+    private int bytesToInt(byte[] bytes) {
+        int entero = 0;
+        for (int i = 3; i > 0; i--) {
+            entero += bytes[i] & 0xFF;
+            entero <<= 8;
+        }
+        entero += bytes[0] & 0xFF;
+        return entero;
     }
 }
